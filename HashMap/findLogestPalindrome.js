8d0429449bbc0b4322a620e23c11f891037c6fb3
Sanjay Jangid <sanjay50986@gmail.com>
2026-05-18 19:13:57 +0530

    findLogestPalindrome

---
+++ HashMap/findLogestPalindrome.js
@@ -0,0 +1,57 @@
+// Problem Description
+// You are given a string consisting of lower and upper case characters.
+// You need to find the length of the longest palindrome which you can create by using the characters from the string.
+// Note: Upper case and lower case characters are different from each other i.e, "Aa" is not a palindrome as 'A' != 'a'.
+
+// Input format
+// There are 2 lines of input
+
+// First line contains the size of the string
+
+// Second line contains the string.
+
+// Output format
+// Print the length of the longest palindrome possible.
+
+// Sample Input 1
+// 4 bbde
+
+// Sample Output 1
+// 3
+
+// Explanation
+// The possible 3 size palindrome strings are :- beb and bdb
+
+// Constraints
+// 1 <= Size of String <= 10^4
+
+
+function longestPalindrome(n, str) {
+    let map = new Map()
+    let length = 0
+    let isPaired = false
+    for(let i = 0; i < str.length; i++) {
+        let ch = str[i]
+        if(map.has(ch)) {
+            let prevFreq = map.get(ch)
+            map.set(ch, prevFreq+1)
+        } else {
+            map.set(ch, 1)
+        }
+    }
+
+    for(let count of map.values()) {
+        if(count % 2 == 0) {
+            length += count
+        } else {
+            length += (count-1)
+            isPaired=true
+        }
+    }
+
+    if(isPaired) {
+        length += 1
+    }
+
+    return length
+}
