cd21c8b0bdcd5d9c3bbe5b312a0eef5db0439291
Sanjay Jangid <sanjay50986@gmail.com>
2026-05-23 17:00:50 +0530

    MiddleNode

--- LinkedList/reverseLinkedList.js
+++ LinkedList/reverseLinkedList.js
@@ -31,3 +31,48 @@
 // 0 <= N <= 10^5
 
 // -10^9 <= A[i] <= 10^9
+
+
+// class Node {
+//   constructor(value) {
+//     this.value = value
+//     this.next = null
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//   }
+  
+//   add(num) {
+//     const temp = new Node(num);
+
+//     if(this.head == null) {
+//       this.head = temp
+//     } else {
+//       let curr = this.head
+
+//       while(curr.next != null) {
+//         curr = curr.next
+//       }
+
+//       curr.next = temp
+//     }
+//   }
+// }
+
+// const list = new LinkedList();
+
+
+function reverseLinkedList(head) {
+    let trail = null, mid = null, curr = head;
+    while(curr != null) {
+          trail = mid;
+          mid = curr;
+          curr = curr.next;
+          mid.next = trail
+    }
+
+    return mid;
+ }
